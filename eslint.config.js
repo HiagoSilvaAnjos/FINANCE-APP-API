9fdfd1b1973a7d068b44019411385cf2d82e9c67
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-06-26 18:36:50 -0300

    chore: add eslint, husky, git-commit-msg-linter, lint-staged

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      // Adicione suas regras aqui
+      "semi": ["error", "always"], // Exemplo: Requer ponto e vírgula no final de cada declaração
+      "quotes": ["error", "double"], // Exemplo: Requer o uso de aspas duplas,
+      "no-var": "error" // Proíbe o uso de var
+    }
+  }
+];
