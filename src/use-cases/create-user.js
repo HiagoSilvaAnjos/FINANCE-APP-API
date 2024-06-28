7dc6fc97ba87a67873c1eb0d4a26c724a1121b9b
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-06-28 09:00:55 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+
+import { PostgresCreateUserRepository } from "../repositories/postgres/create-user";
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        // TODO: Verificar se o e-mail já está em uso
+
+        // Criar ID do usuário
+        const userId = uuidv4();
+
+        // Criptografar a senha
+        const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+        // Inserir o usuário no banco de dados
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword
+        };
+
+
+        // Chamar o repositório
+        const postgresCreateUserRepository = new PostgresCreateUserRepository();
+
+        const createdUser = await postgresCreateUserRepository.execute(user);
+
+        return createdUser;
+    }
+}
