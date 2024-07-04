0bb570694200e88133a179bd86bf697dd549538b
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-04 17:44:49 -0300

    feat: add PostgresDeleteTransactionRepository

---
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -0,0 +1,17 @@
+import { PostgresHelper } from "../../../db/postgres/helper.js";
+
+export class PostgresDeleteTransactionRepository {
+    async execute(transitionId) {
+
+        const deleteTransaction = PostgresHelper.query(
+            `
+            DELETE FROM transactions
+            WHERE id = $1
+            RETURNING *
+            `,
+            [transitionId]
+        );
+
+        return deleteTransaction[0];
+    }
+}
