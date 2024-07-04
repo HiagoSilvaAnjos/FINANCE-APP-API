ad9b5803c9597477169b2515249291c2713c4b8e
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-04 09:20:24 -0300

    feat: add PostgresUpdateTransactionRepository

--- src/repositories/postgres/index.js
+++ src/repositories/postgres/index.js
@@ -6,3 +6,4 @@ export * from "./user/get-user-by-email.js";
 
 export * from "./transaction/create-transaction.js";
 export * from "./transaction/get-transactions-by-id.js";
+export * from "./transaction/update-transaction.js";
