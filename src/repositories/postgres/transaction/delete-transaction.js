7c13c8ced4c40f0585f991df8a7ca2e65b84dba0
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-08 17:41:34 -0300

    feat: ensuring DeleteTransactionsRepository  use Prisma

--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -1,17 +1,11 @@
-import { PostgresHelper } from "../../../db/postgres/helper.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresDeleteTransactionRepository {
     async execute(transitionId) {
-
-        const deleteTransaction = await PostgresHelper.query(
-            `
-            DELETE FROM transactions
-            WHERE id = $1
-            RETURNING *
-            `,
-            [transitionId]
-        );
-
-        return deleteTransaction[0];
+        return await prisma.transaction.delete({
+            where: {
+                id: transitionId
+            }
+        });
     }
 }
