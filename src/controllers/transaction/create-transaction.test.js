ef576b197040499ac26678e90c0ab512c7ee3b30
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-11 09:10:25 -0300

    test: ensuring that CreateTransactionController return 400  when type provided is invalid

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -139,6 +139,20 @@ describe("Create Trasaction Controller", () => {
         expect(result.statusCode).toBe(400);
     });
 
+    it("should return statusCode 400 when type provided is not EXPENSE, EARNING, INVESTMENT", async () => {
+
+        const { createTransactionController } = makeSut();
+
+        const result = await createTransactionController.execute({
+            body: {
+                ...httpRequest.body,
+                type: "invalid_type"
+            }
+        });
+
+        expect(result.statusCode).toBe(400);
+    });
+
     it("Should return 500 if CreateTransactionController throws", async () => {
 
         const { createTransactionUseCaseStub, createTransactionController } = makeSut();
