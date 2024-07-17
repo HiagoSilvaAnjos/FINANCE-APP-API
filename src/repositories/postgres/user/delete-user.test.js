0925cf3a92f78525a75b00565cf0b5e809e766cc
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-17 17:40:24 -0300

    test: ensuring that PostgresDeleteUserRepository delete a user on db

---
+++ src/repositories/postgres/user/delete-user.test.js
@@ -0,0 +1,57 @@
+// import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { prisma } from "../../../../prisma/prisma";
+import { user } from "../../../tests";
+import { PostgresDeleteUserRepository } from "./delete-user";
+// import { UserNotFoundError } from "../../../errors/user.js";
+
+describe("PostgresDeleteUserRepository", () => {
+    it("should delete a user on db", async () => {
+        await prisma.user.create({
+            data: user,
+        });
+
+        const postgresDeleteUserRepository = new PostgresDeleteUserRepository();
+
+        const result = await postgresDeleteUserRepository.execute(user.id);
+
+        expect(result).toStrictEqual(user);
+    });
+
+    // it("should call Prisma with correct params", async () => {
+    //     await prisma.user.create({ data: user });
+    //     const postgresDeleteUserRepository = new PostgresDeleteUserRepository();
+    //     const prismaSpy = jest.spyOn(prisma.user, "delete");
+
+    //     await postgresDeleteUserRepository.execute(user.id);
+
+    //     expect(prismaSpy).toHaveBeenCalledWith({
+    //         where: {
+    //             id: user.id,
+    //         },
+    //     });
+    // });
+
+    // it("should throw generic error if Prisma throws generic error", async () => {
+    //     const postgresDeleteUserRepository = new PostgresDeleteUserRepository();
+    //     jest
+    //         .spyOn(prisma.user, "delete")
+    //         .mockRejectedValueOnce(new Error());
+
+    //     const promise = postgresDeleteUserRepository.execute(user.id);
+
+    //     await expect(promise).rejects.toThrow();
+    // });
+
+    // it("should throw UserNotFoundError if Prisma does not find record to delete", async () => {
+    //     const postgresDeleteUserRepository = new PostgresDeleteUserRepository();
+    //     jest.spyOn(prisma.user, "delete").mockRejectedValueOnce(
+    //         new PrismaClientKnownRequestError("", {
+    //             code: "P2025",
+    //         }),
+    //     );
+
+    //     const promise = postgresDeleteUserRepository.execute(user.id);
+
+    //     await expect(promise).rejects.toThrow(new UserNotFoundError(user.id));
+    // });
+});
