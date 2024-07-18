bb4c61522a39aacb9c01a4ae26dfb8098f24f861
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-18 08:08:21 -0300

    test: ensuring that PostgresUpdateUserRepository should update user on db

---
+++ src/repositories/postgres/user/update-user.test.js
@@ -0,0 +1,26 @@
+import { faker } from "@faker-js/faker";
+import { prisma } from "../../../../prisma/prisma.js";
+import { user as fakeUser } from "../../../tests";
+import { PostgresUpdateUserRepository } from "./update-user.js";
+// import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
+// import { UserNotFoundError } from '../../../errors/user.js'
+
+describe("PostgresUpdateUserRepository", () => {
+    const updateUserParams = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+    };
+
+    it("should update user on db", async () => {
+        const user = await prisma.user.create({ data: fakeUser });
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+
+        const result = await postgresUpdateUserRepository.execute(user.id, updateUserParams);
+
+        expect(result).toStrictEqual(updateUserParams);
+    });
+
+});
