32c97ff1850679249007e7c5e1744ea4b6b42165
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-18 09:35:05 -0300

    test: ensuring that PostgresUpdateTransactionRepository update a transaction on db

---
+++ src/repositories/postgres/transaction/update-transaction.test.js
@@ -0,0 +1,39 @@
+import { faker } from "@faker-js/faker";
+import dayjs from "dayjs";
+import { TransactionType } from "@prisma/client";
+import { prisma } from "../../../../prisma/prisma";
+import { transaction, user } from "../../../tests";
+import { PostgresUpdateTransactionRepository } from "./update-transaction";
+// import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library.js'
+// import { TransactionNotFoundError } from '../../../errors'
+
+describe("PostgresUpdateTransactionRepository", () => {
+    it("should update a transaction on db", async () => {
+        await prisma.user.create({ data: user });
+        await prisma.transaction.create({
+            data: { ...transaction, user_id: user.id },
+        });
+        const postgresUpdateTransactionRepository = new PostgresUpdateTransactionRepository();
+        const params = {
+            id: faker.string.uuid(),
+            user_id: user.id,
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: TransactionType.EXPENSE,
+            amount: Number(faker.finance.amount()),
+        };
+
+        const result = await postgresUpdateTransactionRepository.execute(transaction.id, params);
+
+        expect(result.name).toBe(params.name);
+        expect(result.type).toBe(params.type);
+        expect(result.user_id).toBe(user.id);
+        expect(String(result.amount)).toBe(String(params.amount));
+        expect(dayjs(result.date).daysInMonth()).toBe(
+            dayjs(params.date).daysInMonth(),
+        );
+        expect(dayjs(result.date).month()).toBe(dayjs(params.date).month());
+        expect(dayjs(result.date).year()).toBe(dayjs(params.date).year());
+    });
+
+});
