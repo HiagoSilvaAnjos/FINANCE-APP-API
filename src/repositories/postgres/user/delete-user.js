f11e7e03493aa928fc3b3559a10e83ef8306e3b6
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-18 17:06:52 -0300

    feat: ensuring PostgresDeleteUserRepository throws if Pisma throws

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -1,15 +1,23 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { prisma } from "../../../../prisma/prisma.js";
+import { UserNotFoundError } from "../../../errors/user.js";
 export class PostgresDeleteUserRepository {
     async execute(userId) {
         try {
             return await prisma.user.delete({
                 where: {
-                    id: userId
-                }
+                    id: userId,
+                },
             });
         } catch (error) {
-            console.log(error);
-            return false;
+            if (error instanceof PrismaClientKnownRequestError) {
+                // P2025 = "An operation failed because it depends on one or more records that were required but not found" (from Prisma docs)
+                if (error.code === "P2025") {
+                    throw new UserNotFoundError(userId);
+                }
+            }
+
+            throw error;
         }
     }
 }
