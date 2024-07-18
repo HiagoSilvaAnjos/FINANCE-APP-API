1e4e29b2adbe76ce412b34c4fd5d437face532c1
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-18 17:07:03 -0300

    test: ensuring PostgresDeleteUserRepository throws if Pisma throws

--- src/repositories/postgres/user/delete-user.test.js
+++ src/repositories/postgres/user/delete-user.test.js
@@ -1,6 +1,8 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { prisma } from "../../../../prisma/prisma";
 import { user } from "../../../tests";
 import { PostgresDeleteUserRepository } from "./delete-user";
+import { UserNotFoundError } from "../../../errors/user";
 
 describe("PostgresDeleteUserRepository", () => {
     it("should delete a user on db", async () => {
@@ -27,6 +29,30 @@ describe("PostgresDeleteUserRepository", () => {
                 id: user.id,
             },
         });
+    });
+
+    it("should throw generic error if Prisma throws generic error", async () => {
+        const postgresCreateUserRepository = new PostgresDeleteUserRepository();
+        jest
+            .spyOn(prisma.user, "delete")
+            .mockRejectedValueOnce(new Error());
+
+        const promise = postgresCreateUserRepository.execute(user.id);
+
+        await expect(promise).rejects.toThrow();
+    });
+
+    it("should throw UserNotFoundError if Prisma does not find record to delete", async () => {
+        const postgresCreateUserRepository = new PostgresDeleteUserRepository();
+        jest.spyOn(prisma.user, "delete").mockRejectedValueOnce(
+            new PrismaClientKnownRequestError("", {
+                code: "P2025",
+            }),
+        );
+
+        const promise = postgresCreateUserRepository.execute(user.id);
+
+        await expect(promise).rejects.toThrow(new UserNotFoundError(user.id));
     });
 
 });
