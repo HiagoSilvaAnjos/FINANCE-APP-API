6bd1a87527e2df9c159b7af93c3b9f72af7c2856
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-19 19:27:28 -0300

    test: ensuring that user Controller Factories  return a valid GetUserByIdController instance

---
+++ src/factories/controllers/user.test.js
@@ -0,0 +1,12 @@
+import { GetUserByIdController } from "../../controllers";
+import { makeGetUserByIdController } from "./user";
+
+describe("User Controller Factories", () => {
+    it("should return a valid GetUserByIdController instance", () => {
+        expect(makeGetUserByIdController()).toBeInstanceOf(
+            GetUserByIdController,
+        );
+    });
+
+
+});
