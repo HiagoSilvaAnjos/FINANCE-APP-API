5ab16f346bfc5b0dce291c3a355cc1abb24736cf
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-20 17:39:07 -0300

    test: ensuring that GET /api/transaction?userId should return 200 when fetching transactions successfully

--- src/routes/transaction.e2e.test.js
+++ src/routes/transaction.e2e.test.js
@@ -22,4 +22,24 @@ describe("Transaction Router 2E2 test", () => {
         expect(response.body.amount).toBe(String(transaction.amount));
     });
 
+    it("GET /api/transaction?userId should return 200 when fetching transactions successfully", async () => {
+        const { body: createdUser } = await request(app)
+            .post("/api/users")
+            .send({
+                ...user,
+                id: undefined,
+            });
+
+        const { body: createdTransaction } = await request(app)
+            .post("/api/transactions")
+            .send({ ...transaction, user_id: createdUser.id, id: undefined });
+
+        const response = await request(app).get(
+            `/api/transactions?userId=${createdUser.id}`,
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body[0].id).toBe(createdTransaction.id);
+    });
+
 });
