3a93e6cbfb65a39ddfc48f09fe2ef5a8e3b9dfc7
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-22 08:11:04 -0300

    chore: add swagger initial setup

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from "express";
 
 import { usersRouter, transactionsRouter } from "./routes/index.js";
 
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+
 export const app = express();
 
 app.use(express.json());
@@ -11,3 +15,8 @@ app.use("/api/users", usersRouter);
 
 // Transactions
 app.use("/api/transactions", transactionsRouter);
+
+// Documentação
+const swaggerDocument = JSON.parse(fs.readFileSync(path.join(import.meta.dirname, "../docs/swagger.json"), "utf8"));
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
