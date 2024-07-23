941304717d69ccf3b0aa5c056358b815c019075f
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-23 08:53:18 -0300

    fix: update eslint file

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,26 @@
+// eslint.config.js
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
   {
     languageOptions: {
+      ecmaVersion: 2023, // Usando a versão mais recente compatível com Node.js 20
+      sourceType: "module", // Assume que você está usando módulos ES
       globals: {
         ...globals.node,
-        ...globals.jest
-      }
-    }
+        ...globals.jest,
+      },
+    },
   },
   pluginJs.configs.recommended,
   {
     rules: {
-      // Adicione suas regras aqui
+      // Regras de estilo e melhores práticas
       "semi": ["error", "always"], // Requer ponto e vírgula no final de cada declaração
       "quotes": ["error", "double"], // Requer o uso de aspas duplas
-      "no-var": "error" // Proíbe o uso de var
-    }
-  }
+      "no-var": "error", // Proíbe o uso de var
+      // Outras regras que você desejar
+    },
+  },
 ];
