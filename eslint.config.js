63e00129e5e3405c889c4e6e7892c968fc670337
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-07-23 09:01:26 -0300

    fix: remove unnecessary comments

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,8 @@ import pluginJs from "@eslint/js";
 export default [
   {
     languageOptions: {
-      ecmaVersion: 2023, // Usando a versão mais recente compatível com Node.js 20
-      sourceType: "module", // Assume que você está usando módulos ES
+      ecmaVersion: 2023,
+      sourceType: "module",
       globals: {
         ...globals.node,
         ...globals.jest,
@@ -16,11 +16,9 @@ export default [
   pluginJs.configs.recommended,
   {
     rules: {
-      // Regras de estilo e melhores práticas
       "semi": ["error", "always"], // Requer ponto e vírgula no final de cada declaração
       "quotes": ["error", "double"], // Requer o uso de aspas duplas
       "no-var": "error", // Proíbe o uso de var
-      // Outras regras que você desejar
     },
   },
 ];
